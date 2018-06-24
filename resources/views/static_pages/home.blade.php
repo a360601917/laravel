82b772f00a742fe4c075267af713e91e684a9bb6
wl <360601917@qq.com>
2018-06-25 00:04:34 +0800

    wer

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,5 +2,7 @@
 @section('title','主页')
 @section('content')
   <h1>主页</h1>
-  <a href="<?= route('help')?>">ewerwer</a>
+  <a href="<?= route('help')?>">ewerwer</a><br>
+  <a href="<?= route('create')?>">注册</a><br>
+  
 @stop
