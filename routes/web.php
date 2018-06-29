75ae88d2817fa7704b58e4daabd0e2eebd74c576
wl1 <360601917@qq.com>
2018-06-29 16:13:58 +0800

    ewer

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,9 @@ Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about');
 
-Route::get('signup', 'UsersController@create')->name('create');
+Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
+
+Route::get('login', 'SessionController@create')->name('login');
+Route::post('login', 'SessionController@store')->name('login');
+Route::get('logout', 'SessionController@destroy')->name('logout');
